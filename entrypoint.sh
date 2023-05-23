24fc453daf213a812f0ff360467969601fc2a199
Robert Bendun <robert@bendun.cc>
2023-05-24 01:45:38 +0200

    looool

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
 cd /ium/src
-python3 tf_train.py "$1"
-cp /ium/src/model.keras /github/workspace/
+
+if [ -f /github/workspace/model.keras ]; then
+	# Since model exists we already did the training
+	cp /github/workspace/model.keras /ium/src/
+	python3 tf_test.py "$1"
+else
+	python3 tf_train.py "$1"
+	cp /ium/src/model.keras /github/workspace/
+fi
